c3b3fc63287ebcb4f9d2dc9cce656da7782771ab
AlexisGuilloux <alexisg@tinydigitalfactory.com>
2023-07-03 22:49:32 +0200

    Add song data structure, assets and UI

---
+++ Assets/Scripts/SongData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SongData", menuName = "ScriptableObjects/SongData", order = 0)]
+public class SongData : ScriptableObject
+{
+    public string _titleName;
+    public string _artistName;
+    public string _albumName;
+    public Sprite _albumImage;
+    public AudioClip _audioClip;
+    public Animation _songAnimation;
+    public GameObject _songCharacter;
+}
